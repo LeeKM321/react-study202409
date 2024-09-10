3850d87e40ed4d07f2f2ef6ee24a371f2297f81d
LeeKM321 <stephen4951@gmail.com>
2024-09-10 16:39:17 +0900

    지출내역 조건부 렌더링

--- src/components/Expense/Expenses.js
+++ src/components/Expense/Expenses.js
@@ -15,19 +15,31 @@ const Expenses = ({ expenses }) => {
     setFilteredYear(selectedYear);
   };
 
+  // 고차함수 filter를 따로 분리 -> 필터링 결과가 비었을 경우 없다고 얘기하기 위해서.
+  const filteredItem = expenses.filter(
+    (item) => item.date.getFullYear().toString() === filteredYear,
+  );
+
+  // 조건부 렌더링을 위한 변수 -> 기본값으로 없다고 깔아놓음.
+  let expenseContent = <p>아직 등록된 지출이 없습니다.</p>;
+
+  // 혹시 필터링 된 결과가 하나라도 존재한다면?
+  // 필터링 된 결과를 ExpenseItem으로 맵핑하자. (원래 우리가 하던 일.)
+  if (filteredItem.length > 0) {
+    expenseContent = filteredItem.map((item) => (
+      <ExpenseItem
+        key={item.id} // 반복문을 통해 같은 컴포넌트를 표현할 때, 각각을 구분할 수 있게 해 주는 prop
+        title={item.title}
+        price={item.price}
+        date={item.date}
+      />
+    ));
+  }
+
   return (
     <Card className='expenses'>
       <ExpenseFilter onChangeFilter={filterChangeHandler} />
-      {expenses
-        .filter((item) => item.date.getFullYear().toString() === filteredYear)
-        .map((item) => (
-          <ExpenseItem
-            key={item.id} // 반복문을 통해 같은 컴포넌트를 표현할 때, 각각을 구분할 수 있게 해 주는 prop
-            title={item.title}
-            price={item.price}
-            date={item.date}
-          />
-        ))}
+      {expenseContent}
     </Card>
   );
 };
