ffcd2daa921d5df6eb19434b50267fd18545bd78
LeeKM321 <stephen4951@gmail.com>
2024-09-11 11:16:39 +0900

    styled-components 마무리

--- src/components/CourseGoals/CourseInput.js
+++ src/components/CourseGoals/CourseInput.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './CourseInput.css';
-import Button from '../UI/Button';
+import ButtonComponent from '../UI/Button';
 
 const CourseInput = ({ onAdd }) => {
   const [enteredText, setEnteredText] = useState('');
@@ -31,7 +31,7 @@ const CourseInput = ({ onAdd }) => {
         <label>나의 목표</label>
         <input type='text' value={enteredText} onChange={textChangeHandler} />
       </div>
-      <Button type='submit'>목표 추가하기</Button>
+      <ButtonComponent type='submit'>목표 추가하기</ButtonComponent>
     </form>
   );
 };
--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -2,6 +2,8 @@ import React from 'react';
 import styled from 'styled-components';
 // import './Button.css';
 
+// styled-components가 제공하는 styled를 사용해서 원하는 태그를 생성하고
+// 해당 태그에 디자인을 문자열로 바로 지정할 수 있습니다.
 const Button = styled.button`
   font: inherit;
   padding: 0.5rem 1.5rem;
@@ -23,6 +25,16 @@ const Button = styled.button`
   }
 `;
 
+// styled-components를 사용하면서 props를 처리하고 싶다면
+// 중간 단계의 컴포넌트를 하나 더 추가하면 됩니다.
+const ButtonComponent = ({ type, onclick, children }) => {
+  return (
+    <Button type={type} onClick={onclick}>
+      {children}
+    </Button>
+  );
+};
+
 // 반복되는 태그가 다양한 속성을 가지게 된다면
 // 컴포넌트로 분할해서 속성값을 일괄적으로 처리할 수 있게 하자.
 // const Button = ({ type, onclick, children }) => {
@@ -33,4 +45,4 @@ const Button = styled.button`
 //   );
 // };
 
-export default Button;
+export default ButtonComponent;
