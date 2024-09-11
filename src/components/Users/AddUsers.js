a5143c2116b79c8fed3258bd5d311e5cac121473
LeeKM321 <stephen4951@gmail.com>
2024-09-11 16:46:30 +0900

    useRef Hook 사용

--- src/components/Users/AddUsers.js
+++ src/components/Users/AddUsers.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import Card from '../UI/Card';
 import ButtonComponent from '../UI/Button';
 import styles from './AddUsers.module.css';
@@ -9,30 +9,23 @@ const AddUsers = ({ onAdd }) => {
     이름과 나이를 입력받아서, 가입하기 버튼을 누르면 가입 처리를 해 주세요.
     가입 처리? -> App.js에 있는 USER_LIST에 객체 형태로 추가.
     */
-  const [userValue, setUserValue] = useState({
-    userName: '',
-    age: '',
-  });
+
   // 에러 상태를 관리하는 변수
   const [error, setError] = useState(null);
 
-  const userNameChangeHandler = (e) => {
-    console.log('nameChangeHandler 호출됨!');
-
-    setUserValue((prev) => {
-      return { ...prev, userName: e.target.value };
-    });
-  };
-
-  const ageChangeHandler = (e) => {
-    setUserValue((prev) => {
-      return { ...prev, age: e.target.value };
-    });
-  };
+  // useRef로 기억된 input 요소 가져오기
+  const nameInput = useRef();
+  const ageInput = useRef();
 
   const userSubmitHandler = (e) => {
     e.preventDefault();
-    if (userValue.userName.trim() === '' || userValue.age.trim() === '') {
+    console.log(nameInput);
+    console.log(ageInput);
+
+    if (
+      nameInput.current.value.trim() === '' ||
+      ageInput.current.value.trim() === ''
+    ) {
       setError({
         title: '유효하지 않은 입력값',
         message: '입력값은 공백으로 작성하시면 안됩니다!',
@@ -40,7 +33,7 @@ const AddUsers = ({ onAdd }) => {
       return;
     }
 
-    if (+userValue.age < 1) {
+    if (+ageInput.current.value < 1) {
       setError({
         title: '유효하지 않은 나이의 범위',
         message: '나이는 1 이상의 숫자로 작성해 주세요!',
@@ -48,12 +41,13 @@ const AddUsers = ({ onAdd }) => {
       return;
     }
 
-    onAdd(userValue);
-
-    setUserValue({
-      userName: '',
-      age: '',
+    onAdd({
+      userName: nameInput.current.value,
+      age: ageInput.current.value,
     });
+
+    nameInput.current.value = '';
+    ageInput.current.value = '';
   };
 
   return (
@@ -68,19 +62,9 @@ const AddUsers = ({ onAdd }) => {
       <Card className={styles.input}>
         <form onSubmit={userSubmitHandler}>
           <label htmlFor='username'>이름</label>
-          <input
-            id='username'
-            type='text'
-            onChange={userNameChangeHandler}
-            value={userValue.userName}
-          />
+          <input id='username' type='text' ref={nameInput} />
           <label htmlFor='age'>나이</label>
-          <input
-            id='age'
-            type='number'
-            onChange={ageChangeHandler}
-            value={userValue.age}
-          />
+          <input id='age' type='number' ref={ageInput} />
           <ButtonComponent type='submit'>가입하기</ButtonComponent>
         </form>
       </Card>
